58e4463fb62416de3600dcbea3e4acbebe3e667f
Sebastian Erhardt <git@erhardt.net>
2022-03-30 17:42:24 +0200

    ingest bulk

---
+++ pkg/insert/postgres_insert_bulk.go
@@ -0,0 +1,27 @@
+package insert
+
+import (
+	"github.com/max-planck-innovation-competition/go-patstat/connections"
+	log "github.com/sirupsen/logrus"
+)
+
+// BulkReadFile reads a file and creates the db entries in batches
+func BulkReadFile(filePath string) {
+	logger := log.WithFields(log.Fields{"file": filePath})
+	logger.Info("reading file")
+	// get the table name of the file
+	tableName, err := File2Table(filePath)
+	if err != nil {
+		logger.Fatal(err)
+		return
+	}
+	logger.WithField("table_name", tableName).Info("sql table name")
+	// copy csv
+	logger.Info("start copying csv")
+	err = connections.SQLClient.Exec(`COPY ` + tableName + ` FROM '` + filePath + `' WITH CSV HEADER;`).Error
+	if err != nil {
+		logger.Fatal(err)
+		return
+	}
+	logger.Info("successfully finished")
+}
